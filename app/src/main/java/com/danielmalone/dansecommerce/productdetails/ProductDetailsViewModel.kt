794120d5c0fa84e84686a4a4d556bd422e88917a
Admin <arab1997>
2020-08-05 14:08:52 +0500

    ORGINAL APP ECOMMERCE APP no database

--- app/src/main/java/com/danielmalone/dansecommerce/productdetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/productdetails/ProductDetailsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.danielmalone.dansecommerce.model.Product
+import com.danielmalone.dansecommerce.repos.ProductsRepository
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,12 +16,13 @@ class ProductDetailsViewModel : ViewModel() {
 
     fun fetchProductDetails(productTitle: String) {
         viewModelScope.launch(Dispatchers.Default) {
-            val json =
+           /* val json =
                 URL("https://gist.githubusercontent.com/danielmalone/df34a33a06e985d85f2ba7f6e635c600/raw/df0aeaea2def4aa1c9b0f266d032b6733c3fbaaa/shopping_products.json").readText()
             val list = Gson().fromJson(json, Array<Product>::class.java).toList()
             val product = list.first { it.title == productTitle }
 
-            productDetails.postValue(product)
+           productDetails.postValue(product)*/
+            productDetails.postValue(ProductsRepository().fetchProduct(productTitle))
         }
     }
 }
--- app/src/main/java/com/danielmalone/dansecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/repos/ProductsRepository.kt
@@ -22,26 +22,31 @@ class ProductsRepository {
         return retrofit().fetchAllProducts()
     }
 
-    fun getAllProducts(): Single<List<Product>> {
-        return Single.create<List<Product>> {
-            it.onSuccess(fetchProducts())
-        }
+    suspend fun fetchProduct(productTitle: String): Product {
+        return fetchAllProductsRetrofit().first { it.title == productTitle }
     }
+
+
+    /* fun getAllProducts(): Single<List<Product>> {
+         return Single.create<List<Product>> {
+             it.onSuccess(fetchProducts())
+         }
+     }*/
 
     suspend fun searchForProducts(term: String): List<Product> {
         return fetchAllProductsRetrofit().filter { it.title.contains(term, true) }
     }
 
-    fun getProductByName(name: String): Single<Product> {
+  /*  fun getProductByName(name: String): Single<Product> {
         return Single.create<Product> {
             val product = fetchProducts().first { it.title == name }
             it.onSuccess(product)
         }
-    }
+    }*/
 
-    fun fetchProducts(): List<Product> {
+   /* fun fetchProducts(): List<Product> {
         val json =
             URL("https://gist.githubusercontent.com/danielmalone/df34a33a06e985d85f2ba7f6e635c600/raw/df0aeaea2def4aa1c9b0f266d032b6733c3fbaaa/shopping_products.json").readText()
         return Gson().fromJson(json, Array<Product>::class.java).toList()
-    }
+    }*/
 }
